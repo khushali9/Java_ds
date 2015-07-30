f19882706b794dfafa40130b78edc5737e1237b4
Khushali Desai <khushali.desai@sjsu.edu>
2015-07-30 07:55:40 -0700

    infix to postfix work

--- src/_13_Infixpostfix.java
+++ src/_13_Infixpostfix.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
 /**
  * Created by khushali on 4/7/15.
  */
@@ -19,6 +24,127 @@ public class _13_Infixpostfix {
 
     //8.	At the end of the expression, pop and print all operators on the stack. (No parentheses should remain.)
 
+    static int prec(char ch)
+    {
+        switch (ch)
+        {
+            case '+':
+            case '-':
+                return 1;
+
+            case '*':
+            case '/':
+                return 2;
+
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+     public static void main(String args[]) throws IOException{
+
+         System.out.println("Enter the word you want to reverse");
+
+         InputStreamReader isr = new InputStreamReader(System.in);
+         BufferedReader br = new BufferedReader(isr);
+         String word = br.readLine();
+         String output="";
+         int word_size = word.length();
+
+         Stack st=new Stack();
+         for(int i=0;i<word_size;i++) {
+             char ch=word.charAt(i);
+             switch(ch){ //open switch
+                 case '(':
+                     st.push(ch);
+                     break;
+                 case ')':
+                     while(!st.isEmpty()){
+                         if(!(st.peek().toString().equals('('))) {
+                             output += st.pop().toString();
+                         }
+                         else
+                         {
+                             st.pop();
+                             break;
+                         }
+                     }
+                     break;
+                 //while
+                 case'+':
+                 case'-':
+                     if(st.isEmpty()){
+                         st.push(ch);
+                     }
+                              else {
+                                  //while (!st.isEmpty())
+                                  {
+                                      /**if ((st.peek().toString().equals('('))) {
+                                       st.push(ch);
+                                       }
+                                       if (prec(st.peek().toString().charAt(0)) < prec(ch)) {
+                                       st.push(ch);
+                                       }*/
+
+                                      if(prec(st.peek().toString().charAt(0))>=prec(ch)){
+                                      //if (st.peek().toString().equals('*') || st.peek().toString().equals('/')||st.peek().toString().equals('+')) {
+                                         System.out.println("hu"+ch+st.peek());
+                                          output += st.pop().toString();
+                                          //st.pop();
+                                      } else {
+                                          break;
+                                      }
+
+                                  }
+                      }
+                     st.push(ch);
+                     break;
+                 case'*':
+                 case'/':
+                     if(st.isEmpty()){
+                         st.push(ch);
+                     }
+                     else {
+                         //while (!st.isEmpty())
+                         {
+                             /**if ((st.peek().toString().equals('('))) {
+                                 st.push(ch);
+                             }*/
+
+                             if (prec(st.peek().toString().charAt(0))>=prec(ch)) {
+                                 System.out.println("hi");
+                                 output += st.pop().toString();
+                                 st.pop();
+                             }
+                             if (prec(st.peek().toString().charAt(0)) < prec(ch)) {
+                                 System.out.println("hu");
+                                 st.push(ch);
+                             }
+                             else {
+                                 break;
+                             }
+
+                         }
+                     }
+                     st.push(ch);
+                     break;
+
+                 default:
+                     //System.out.println(ch);
+                     output+=ch;
+                    // st.push(ch);
+                     break;
 
 
+
+
+             }//close switch
+
+         }
+         if (!st.isEmpty()){
+             output += st.pop().toString();
+         }
+         System.out.println(output);
+
+     }
 }
