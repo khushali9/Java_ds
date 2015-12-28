6afc86abcd62ae729a8ab980c41a332fc42eb2c8
Khushali Desai <khushali.desai@sjsu.edu>
2015-12-27 16:09:53 -0800

    Solving string problems

---
+++ src/Algorithm/Hackerank/_01_String_Java.java
@@ -0,0 +1,37 @@
+package Algorithm.Hackerank;
+import java.io.*;
+import java.util.*;
+/**
+ * Created by khushali on 12/27/15.
+ */
+public class _01_String_Java {
+
+//Accepted
+    //my mistakes string mathods on Char, take care of lexico if and is + then A is bigger
+
+
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            String A=sc.next();
+            String B=sc.next();
+
+
+            //code for length
+            int m=A.length()+B.length();
+            System.out.println(m);
+
+            //code for lexicograph
+            if (A.compareTo(B)>0)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+
+            String a=A.substring(0,1).toUpperCase().concat(A.substring(1,A.length()));
+            String b=B.substring(0, 1).toUpperCase().concat(B.substring(1, B.length()));
+
+            System.out.println(a+" "+b);
+
+        }
+    }
+
---
+++ src/Algorithm/Hackerank/_02_String_Sub_lex.java
@@ -0,0 +1,16 @@
+package Algorithm.Hackerank;
+
+/**
+ * Created by khushali on 12/27/15.
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class _02_String_Sub_lex {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
